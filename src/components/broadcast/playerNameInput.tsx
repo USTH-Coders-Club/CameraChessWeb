0b753cb395f06fb561cd2cae656d504901b6aaa0
unknown <leviettrung.social@gmail.com>
2024-07-29 23:41:10 +0700

    add: lock feat

--- src/components/broadcast/playerNameInput.tsx
+++ src/components/broadcast/playerNameInput.tsx
@@ -1,14 +1,37 @@
 import { SetString } from "../../types";
+import { useState } from "react";
 
 const PlayerNameInput = ({ setPlayerBlack, setPlayerWhite }: { setPlayerBlack: SetString, setPlayerWhite: SetString }) => {
+  const [isLocked, setIsLocked] = useState(false);
+
+  const handleLockClick = () => {
+    setIsLocked(!isLocked);
+  };
 
   return (
-    <div className="text-white">
-
-      <input type="text" placeholder="Black" onChange={(e) => setPlayerBlack(e.target.value)} />
-      <input type="text" placeholder="White" onChange={(e) => setPlayerWhite(e.target.value)} />
+    <div 
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      gap: "0.25rem"
+    }}
+    className="text-white">
+      <input 
+      className="form-control-sm"
+      type="text" placeholder="Black" onChange={(e) => setPlayerBlack(e.target.value)} disabled={isLocked} />
+      <input 
+      className="form-control-sm"
+      type="text" placeholder="White" onChange={(e) => setPlayerWhite(e.target.value)} disabled={isLocked} />
+      <button
+        className="btn btn-dark btn-sm btn-outline-light w-100"
+        onClick={handleLockClick}>{
+          isLocked ? "Unlock" : "Lock"
+        }</button>
     </div>
-  )
+  );
 }
 
 export default PlayerNameInput;
